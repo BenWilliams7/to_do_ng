9a416dc2ba60d55e311133e3df6c66d448e61dd9
Ben Williams and Sara Schultz <ben@>
2017-06-07 16:54:21 -0700

    fix bug and near completion of editing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,30 +1,20 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   //@Component is known as a decorator. Decorators are special pieces of syntax that configure elements like components. The decorator above tells Angular that the code following it defines a new component and should therefore have the functionalities outlined by Angular's own Component code imported at the top of the file.
   selector: 'app-root',
   //selector: 'app-root' defines what tag corresponds with the component. Because we want this root component rendered within the <app-root></app-root> tags in index.html, the component's selector property must also be app-root. A component's selector determines what its corresponding tags will look like.
   template: `
-    <div class="container">
-      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-      <h3>{{currentFocus}}</h3>
-      <ul>
-        <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
-      </ul>
-      <div *ngIf="selectedTask">
-        <h3>{{selectedTask.description}}</h3>
-        <p>Task Complete? {{selectedTask.done}}</p>
-        <h3>Edit Task</h3>
-        <label>Enter Task Description:</label>
-        <input [(ngModel)]="selectedTask.description">
-        <label>Enter Task Priority (1-3):</label>
-        <br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-        <button (click)="finishedEditing()">Done</button>
-      </div>
-    </div>
+  <div class="container">
+    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
+
+
+    <hr>
+    <edit-task [childSelectedTask]="selectedTask"></edit-task>
+  </div>
   `
   //The template portion (above) provides the HTML that will be displayed wherever the component is placed. Because we've defined our root component's selector property as app-root, the HTML listed in the template property will be rendered wherever the <app-root></app-root> tags are placed. We placed ours directly in the <body> tags of index.html. So, the HTML <h1>My First Angular 2 App</h1> will render in those same <body> tags.
 })
@@ -35,41 +25,19 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course', 3),
-    new Task('Begin brainstorming possible JavaScript group projects', 2),
-    new Task('Add README file to last few Angular repos on GitHub', 2)
-  ];
   selectedTask = null;
-  
-  finishedEditing(){
-    this.selectedTask = null;
-  }
+
+  masterTaskList: Task[] = [
+    new Task('Resolve existential angst', 1),
+    new Task('Stare into void until it blinks', 2),
+    new Task('Paint the black hole blacker', 3)
+  ];
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
   }
 
-  isDone(clickedTask: Task) {
-    if(clickedTask.done === true) {
-      alert("This task is done!");
-    } else {
-      alert("This task is not done. Better get to work slacker.");
-    }
-  }
-  priorityColor(currentTask) {
-    if (currentTask.priority===3){
-      return "bg-danger";
-    } else if (currentTask.priority===2) {
-      return "bg-warning";
-    } else {
-      return "bg-info";
-    }
+  finishedEditing(){
+    this.selectedTask = null;
   }
 }
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public priority: number) { }
-}
-//The class annotation, where our template property resides, determines how a component appears, whereas the class declaration defines how it behaves.
